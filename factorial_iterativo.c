3abfee5ac2b70edb61e46e6bf2faba8505419f8b
Maria Parra <maria.parra05@usa.edu.co>
2025-03-06 14:11:58 -0500

    Update factorial_iterativo.c

--- factorial_iterativo.c
+++ factorial_iterativo.c
@@ -1,1 +1,24 @@
+#include <stdio.h>
 
+// Función para calcular el factorial de un número de forma iterativa
+unsigned long long factorial(int n) {
+    unsigned long long resultado = 1;
+    for (int i = 1; i <= n; i++) {
+        resultado *= i;
+    }
+    return resultado;
+}
+
+int main() {
+    int numero;
+    printf("Ingrese un número entero: ");
+    scanf("%d", &numero);
+    
+    if (numero < 0) {
+        printf("El factorial de un número negativo no está definido.\n");
+    } else {
+        printf("El factorial de %d es %llu\n", numero, factorial(numero));
+    }
+    
+    return 0;
+}
